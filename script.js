3ea217e0f3ca528577f6c9b0d443baaec4302efa
Ankit Dhiman <63338248+ankitdhiman47704@users.noreply.github.com>
2023-01-19 19:42:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+let word = document.getElementById("evaluatedText");
+let count = document.getElementById("letterCount");
+
+
+word.addEventListener("input" , countlength);
+ function countlength(){
+      
+let evaluated_word = word.value;
+let length = evaluated_word.length;
+count.innerText = length;
+ }
